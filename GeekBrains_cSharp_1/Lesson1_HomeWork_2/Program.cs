0d473842193ed2b469301e911b4bf3f2cd930ee6
pirozhok <pirozhok1985@gmail.com>
2020-11-26 06:57:16 +1000

    Lesson_1 task_1 task_2

---
+++ GeekBrains_cSharp_1/Lesson1_HomeWork_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1_HomeWork_2
+{
+    class Program
+    {
+        //Программа ИМТ
+        static void Main(string[] args)
+        {
+            double h, m, I;
+            Console.Write("Укажите ваш рост в метрах: ");
+            h = double.Parse(Console.ReadLine());
+
+            Console.Write("Укажите ваш вес: ");
+            m = double.Parse(Console.ReadLine());
+
+            I = m / (h * h);
+
+            Console.WriteLine("Ваш индекс массы тела: {0:f3}",I);
+            Console.ReadLine();
+        }
+    }
+}
