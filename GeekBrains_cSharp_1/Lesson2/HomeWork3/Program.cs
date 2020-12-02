b79c3cff7676aa606315378f38dcfb9a25ae6629
pirozhok <pirozhok1985@gmail.com>
2020-12-02 15:40:02 +1000

    Lesson2 HomeWork3

---
+++ GeekBrains_cSharp_1/Lesson2/HomeWork3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    /*Анисимов. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Введите желаемое количество чисел. Ввод завершится, если будет набран 0");
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number == 0) break;
+                if (number % 2 != 0 && number > 0)
+                    sum += number;  
+            }
+            Console.WriteLine($"Сумма всех нечётных положительных чисел равна {sum}");
+            Console.ReadLine();
+        }
+    }
+}
