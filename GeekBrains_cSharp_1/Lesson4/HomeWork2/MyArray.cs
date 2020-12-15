acdd3a446155e94751c099d5386588bab24cf834
pirozhok <pirozhok1985@gmail.com>
2020-12-15 18:21:09 +1000

    Lesson4 HomeWork2

---
+++ GeekBrains_cSharp_1/Lesson4/HomeWork2/MyArray.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HomeWork2
+{
+    class MyArray
+    {
+        private int[] arr;
+        private int length;
+        private int startValue;
+        private int step;
+        private int sum;
+        private int maxCount;
+
+        //Индексатор
+        public int this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+        //свойство Sum
+        public int Sum
+        {
+            get
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                return sum;
+            }
+        }
+
+        //Свойство MaxCount
+        public int MaxCount
+        {
+            get 
+            {
+                Array.Sort(arr);
+                int max = arr[arr.Length - 1];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == max)
+                        maxCount++;
+                }
+                return maxCount;
+            }
+        }
+
+        //Свойство Length
+        public int Length { get=>arr.Length;}
+
+        //Свойство GetArray
+
+        public ref int[] GetArray
+        {
+            get 
+            {
+                return ref arr;
+            }
+        }
+
+        //Конструктор 1
+        public MyArray(int length, int sVal, int stp)
+        {
+            arr = new int[length];
+            arr[0] = sVal;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = arr[i - 1] + stp;
+            }
+        }
+        //Конструктор 2
+        public MyArray(int length)
+        {
+            arr = new int[length];
+        }
+
+        //Конструктор 3
+        public MyArray(string fileName)
+        {
+            string[] ar = File.ReadAllLines(fileName);
+            arr = new int[ar.Length];
+            for (int i = 0; i < ar.Length; i++) 
+            {
+                int.TryParse(ar[i], out arr[i]);
+            }
+        }
+        //Метод Inverse
+        public void Inverse()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= -1;
+            }
+        }
+
+        //Метод записи массива в файл
+        public static void WriteToFile(MyArray item, String filename)
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            for (int i = 0; i < item.Length; i++)
+            {
+                sw.WriteLine(item[i]);
+            }
+            sw.Close();
+        }
+
+        //Метод Multi
+
+        public void Multi(int val)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= val;
+            }
+        }
+
+        //Метод Merge
+        public static MyArray Merge(MyArray arr1, MyArray arr2)
+        {
+            int newlength = arr1.Length + arr2.Length;
+            MyArray newArr = new MyArray(newlength);
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                newArr[i] = arr1[i];
+            }
+            int l = arr1.Length;
+            for (int j = 0; j < arr2.Length; j++)
+            {
+                newArr[l] = arr2[j];
+                l++;
+            }
+            return newArr;
+        }
+
+        //Метод Copy
+        public static void Copy(MyArray arr1, ref MyArray arr2)
+        {
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+        }
+
+        //Метод Resize
+
+        public static void Resize(ref MyArray item, int length)
+        {
+            MyArray tmp = new MyArray(item.Length);
+            MyArray.Copy(item, ref tmp);
+
+            item = new MyArray(length);
+            MyArray.Copy(tmp, ref item);
+        }
+
+    }
+}
---
+++ GeekBrains_cSharp_1/Lesson4/HomeWork2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    /*Анисимов. а) Дописать класс MyArray для работы с одномерным массивом. Реализовать конструктор, создающий массив заданной размерности и заполняющий массив числами от начального значения с заданным шагом. Создать свойство Sum, которые возвращают сумму элементов массива, метод Inverse, меняющий знаки у всех элементов массива, Метод Multi, умножающий каждый элемент массива на определенное число, свойство MaxCount, возвращающее количество максимальных элементов, создать индексирующее свойство, свойство Length, возвращающая длину массива, метод Merge объединяющий два массива в один и возвращающий объединенный массив, метод MyArray Copy(), возвращающий копию массива, метод MyArray Resize(int newSize) изменяющий размер массива, свойство GetArray с помощью которого можно получить ссылку на массив. В Main продемонстрировать работу класса.
+б)*Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Инициализируем массив arr");
+            MyArray arr = new MyArray(10, 100, 10);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("Сумма всех элементов массива равна: {0}", arr.Sum);
+            Console.WriteLine("Применяем инверсию");
+            arr.Inverse();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine("Используем метод Multi");
+            arr.Multi(10);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine("Инициализируем массив arr1 и заполняем его рандомными значениями в диапазоне от 10 до 20");
+            MyArray arr1 = new MyArray(20);
+            Random rand = new Random();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = rand.Next(10,20);
+            }
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+
+            Console.WriteLine("Количество элементов с максимальным значением равно {0}",arr1.MaxCount);
+
+            Console.WriteLine("Используем метод Merge");
+            MyArray newArr = MyArray.Merge(arr, arr1);
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                Console.WriteLine(newArr[i]);
+            }
+
+            Console.WriteLine("Инициализируем массив arr2");
+            MyArray arr2 = new MyArray(10);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+            Console.WriteLine("Копируем массив arr в массив arr2");
+            MyArray.Copy(arr, ref arr2);
+          
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+            Console.WriteLine("Изменяем размер массива arr2 с 10 на 20");
+            MyArray.Resize(ref arr2, 20);
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+            Console.WriteLine("Ссылка на массив arr2: {0}",arr2.GetArray);
+
+            string fileName = @"C:\Users\Pirozhok\Downloads\test.txt";
+            MyArray.WriteToFile(arr2, fileName);
+
+            Console.WriteLine("Создадим массив из файла");
+            string file = @"C:\Users\Pirozhok\Downloads\test.txt";
+            MyArray arr3 = new MyArray(file);
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                Console.WriteLine(arr3[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
