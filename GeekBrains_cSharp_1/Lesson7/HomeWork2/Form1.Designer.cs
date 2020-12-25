ffd2eb4e2ccca007f4893751fb0a9265a6094ac5
Pirozhok1985 <pirozhok1985@gmail.com>
2020-12-26 01:19:23 +1000

    Lesson7 HomeWork2

---
+++ GeekBrains_cSharp_1/Lesson7/HomeWork2/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace HomeWork2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblResultFieldName = new System.Windows.Forms.Label();
+            this.lblTries = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(18, 40);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber.TabIndex = 0;
+            this.tbNumber.TextChanged += new System.EventHandler(this.tb1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResultFieldName);
+            this.groupBox1.Controls.Add(this.lblTries);
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnOK);
+            this.groupBox1.Controls.Add(this.lblHeader);
+            this.groupBox1.Controls.Add(this.tbNumber);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 191);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblResultFieldName
+            // 
+            this.lblResultFieldName.AutoSize = true;
+            this.lblResultFieldName.Location = new System.Drawing.Point(210, 40);
+            this.lblResultFieldName.Name = "lblResultFieldName";
+            this.lblResultFieldName.Size = new System.Drawing.Size(59, 13);
+            this.lblResultFieldName.TabIndex = 1;
+            this.lblResultFieldName.Text = "Результат";
+            // 
+            // lblTries
+            // 
+            this.lblTries.AutoSize = true;
+            this.lblTries.Location = new System.Drawing.Point(210, 131);
+            this.lblTries.Name = "lblTries";
+            this.lblTries.Size = new System.Drawing.Size(35, 13);
+            this.lblTries.TabIndex = 1;
+            this.lblTries.Text = "label2";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(210, 82);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "label1";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(18, 126);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(18, 82);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(15, 16);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(81, 13);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "Введите число";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(380, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem1});
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.newGameToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem.Text = "New Game";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.tsmNewGame_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(129, 6);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(132, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.tsmExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 215);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Label lblResultFieldName;
+        private System.Windows.Forms.Label lblTries;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ GeekBrains_cSharp_1/Lesson7/HomeWork2/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork2
+{
+    public partial class Form1 : Form
+    {
+        GuessNum number;
+        int value;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbNumber.TextChanged += tb1_TextChanged;
+            if (number == null) 
+            { 
+                tbNumber.Enabled = false;
+                btnOK.Enabled = false;
+                btnReset.Enabled = false;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb1_TextChanged(object sender, EventArgs e)
+        {
+            int.TryParse(tbNumber.Text, out value);
+        }
+
+        private void tsmNewGame_Click(object sender, EventArgs e)
+        {
+            number = new GuessNum();
+            tbNumber.Enabled = true;
+            btnOK.Enabled = true;
+            btnReset.Enabled = true;
+            lblTries.Text = number.Tries.ToString();
+            tbNumber.Text = null;
+            lblResult.Text = null;
+        }
+
+        private void tsmExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            number.Compare(value);
+            lblResult.Text = number.Result;
+            StatusUpdate();
+            if (lblTries.Text == 0.ToString()) 
+            {
+                tbNumber.Enabled = false;
+                btnOK.Enabled = false;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbNumber.Enabled = true;
+            tbNumber.Text = null;
+            btnOK.Enabled = true;
+            number = new GuessNum();
+            StatusUpdate();
+        }
+        void StatusUpdate()
+        {
+            lblResult.Text = number.Result;
+            lblTries.Text = number.Tries.ToString();
+        }
+    }
+}
